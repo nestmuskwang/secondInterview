817185780fa0736ca4a406ff4154c3647b45400a
Wangsg <Wangsg@qq.com>
2020-06-08 20:17:04 +0800

    java能力提升

---
+++ bigcomparny/src/main/java/com/atguigu/cloud/interview/com/atguigu/cloud/interview/jvm/T1.java
@@ -0,0 +1,13 @@
+package com.atguigu.cloud.interview.com.atguigu.cloud.interview.jvm;
+
+/**
+ * @author ：wangsg
+ * @date ： 2020/6/8 20:03
+ */
+public class T1 {
+    volatile  int n = 0;
+
+    public void add(){
+        n ++;
+    }
+}
---
+++ bigcomparny/src/main/java/com/atguigu/cloud/interview/com/atguigu/cloud/interview/jvm/T2.java
@@ -0,0 +1,13 @@
+package com.atguigu.cloud.interview.com.atguigu.cloud.interview.jvm;
+
+import javax.activation.MailcapCommandMap;
+
+/**
+ * @author ：wangsg
+ * @date ： 2020/6/8 20:15
+ */
+public class T2 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
